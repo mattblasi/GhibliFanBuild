103ea5698c5d65ad3fd3058576f919ee1e15acd4
mattblasi <mattblasi@me.com>
2022-03-13 21:27:11 -0400

    1 file modified

--- src/views/Admin/AdminEdit.js
+++ src/views/Admin/AdminEdit.js
@@ -7,6 +7,7 @@ const AdminEdit = ({ Admin: { details, people, photos } }) => {
   const [hasChange, setChange] = useState(false);
   const [photoList, addPhotos] = useState([]);
   const [mov, setMov] = useState();
+
   const formInputs = ['title', 'og_title_rm', 'og_title_jp', 'poster'];
 
   function handleValueChange(field, val) {
@@ -35,91 +36,94 @@ const AdminEdit = ({ Admin: { details, people, photos } }) => {
   }
 
   useEffect(() => {
-    addPhotos([]);
     setMov({
       details: { ...details },
       people: { ...people },
       photos: [...photos],
     });
+    addPhotos([]);
     setLoading(false);
+    let removals = document.querySelectorAll('.gallery-image.remove');
+    removals.forEach(img => img.classList.remove('remove'));
   }, [details, people, photos]);
 
-  if (!mov) return <div />;
   return (
     <div className="admin-edit" data-id={details.id}>
       <CSSTransition
-        in={!isLoading}
+        in={!isLoading && mov}
         timeout={1000}
         classNames="page"
         unmountOnExit
       >
-        <p>EDIT MODE {hasChange && <span> - [CHANGED]</span>}</p>
-        <h2>
-          {details.title} <span>{details.id}</span>
-        </h2>
+        <React.Fragment>
+          <p>EDIT MODE {hasChange && <span> - [CHANGED]</span>}</p>
+          <h2>
+            {details.title} <span>{details.id}</span>
+          </h2>
 
-        {mov && (
-          <React.Fragment>
-            <div className="admin-edit--form">
-              {formInputs.map((item) => {
-                return (
-                  <div className="row" id={`field-${item}`} key={`field-${item}`}>
-                    <label htmlFor={item}>Field: {item}</label>
-                    <input
-                      id={item}
-                      name={item}
-                      type="text"
-                      value={mov.details[item]}
-                      onChange={(e) => handleValueChange(item, e.target.value)}
-                    />
-                  </div>
-                );
-              })}
-            </div>
-            <div className="gallery">
-              <div className="gallery-remove">
-                <h2>Photos Removal List</h2>
-                {photoList.length > 0 && (
-                  <ul>
-                    {photoList.length > 0 &&
-                      photoList.map((p, i) => (
-                        <li key={`images-${i}`}>
-                          <button onClick={() => removeToBeRemoved(p)}>x</button>
-                          {p}
-                        </li>
-                      ))}
-                  </ul>
-                )}
-                {photos.length <= 0 && <p>no photos to delete</p>}
+          {mov && (
+            <React.Fragment>
+              <div className="admin-edit--form">
+                {formInputs.map((item) => {
+                  return (
+                    <div className="row" id={`field-${item}`} key={`field-${item}`}>
+                      <label htmlFor={item}>Field: {item}</label>
+                      <input
+                        id={item}
+                        name={item}
+                        type="text"
+                        value={mov.details[item]}
+                        onChange={(e) => handleValueChange(item, e.target.value)}
+                      />
+                    </div>
+                  );
+                })}
               </div>
-              <div className="gallery-images">
-                {mov.photos.length > 0 &&
-                  mov.photos.map((p, i) => {
-                    return (
-                      <article
-                        id={`images-${i}`}
-                        className="gallery-image"
-                        key={`images-${i}`}
-                        style={{
-                          backgroundImage: `url(${p})`,
-                        }}
-                      >
-                        <img src={p} style={{ display: 'none' }} />
-                        <button
-                          className="gallery-button"
-                          onClick={() => {
-                            addToBeRemoved({ p }, `images-${i}`);
+              <div className="gallery">
+                <div className="gallery-remove">
+                  <h2>Photos Removal List</h2>
+                  {photoList.length > 0 && (
+                    <ul>
+                      {photoList.length > 0 &&
+                        photoList.map((p, i) => (
+                          <li key={`images-${i}`}>
+                            <button onClick={() => removeToBeRemoved(p)}>x</button>
+                            {p}
+                          </li>
+                        ))}
+                    </ul>
+                  )}
+                  {photos.length <= 0 && <p>no photos to delete</p>}
+                </div>
+                <div className="gallery-images">
+                  {mov.photos.length > 0 &&
+                    mov.photos.map((p, i) => {
+                      return (
+                        <article
+                          id={`images-${i}`}
+                          className="gallery-image"
+                          key={`images-${i}`}
+                          style={{
+                            backgroundImage: `url(${p})`,
                           }}
                         >
-                          remove
-                        </button>
-                      </article>
-                    );
-                  })}
+                          <img src={p} style={{ display: 'none' }} />
+                          <button
+                            className="gallery-button"
+                            onClick={() => {
+                              addToBeRemoved({ p }, `images-${i}`);
+                            }}
+                          >
+                            remove
+                          </button>
+                        </article>
+                      );
+                    })}
+                </div>
               </div>
-            </div>
-          </React.Fragment>
-        )}
+            </React.Fragment>
+          )}
+        </React.Fragment>
       </CSSTransition>
     </div>
   );
